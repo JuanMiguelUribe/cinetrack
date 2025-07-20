a4c7157cd69c87ce12cb063296b41ca271ad3c85
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-19 20:11:22 -0500

    Navigation bar implemets

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
-import 'package:cinetrack/presentation/providers/movies/movies_providers.dart';
+import 'package:cinetrack/presentation/providers/providers.dart';
+import 'package:cinetrack/presentation/widgets/shared/botton_nav_with_animation.dart';
 import 'package:cinetrack/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,7 +10,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _HomeView());
+    return Scaffold(
+      body: _HomeView(),
+      bottomNavigationBar: BottonNavWithAnimation(),
+    );
   }
 }
 
@@ -30,18 +34,15 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
     return Column(
       children: [
         CustomAppbar(),
-
-        Expanded(
-          child: ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[index];
-              return ListTile(title: Text(movie.title));
-            },
-          ),
+        MoviesSlideshow(movies: slideShowMovies, showTitle: true),
+        MovieHorizontalListView(
+          movies: nowPlayingMovies,
+          title: "En Cines",
+          subtitle: DateFormat('EEEE, d MMMM', 'es_ES').format(DateTime.now()),
         ),
       ],
     );
