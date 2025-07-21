9181907407e402f74b03074fc3ca43b16fad710a
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-21 17:44:21 -0500

    Human format integrado, para mostrar la fecha actual

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinetrack/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es', null);
 
   runApp(ProviderScope(child: const MainApp()));
 }
