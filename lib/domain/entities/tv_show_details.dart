b92a983875ef70e79e3749b0cff404b1d407bfd8
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-23 18:47:44 -0500

    enitidad de details de tv show creada

---
+++ lib/domain/entities/tv_show_details.dart
@@ -0,0 +1,45 @@
+class TvShowDetails {
+  final int id;
+  final String name;
+  final bool? adult;
+  final String? originalName;
+  final String overview;
+  final String posterPath;
+  final String? backdropPath;
+  final DateTime? firstAirDate;
+  final DateTime? lastAirDate;
+  final double? popularity;
+  final bool? inProduction;
+  final String originalLanguage;
+  final double voteAverage;
+  final int voteCount;
+  final int? numberOfSeasons;
+  final int? numberOfEpisodes;
+  final String? homepage;
+  final String? tagline;
+  final String status;
+  final String type;
+
+  TvShowDetails({
+    this.popularity,
+    this.adult,
+    this.originalName,
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.posterPath,
+    this.backdropPath,
+    this.firstAirDate,
+    this.lastAirDate,
+    this.inProduction,
+    required this.originalLanguage,
+    required this.voteAverage,
+    required this.voteCount,
+    this.numberOfSeasons,
+    this.numberOfEpisodes,
+    this.homepage,
+    this.tagline,
+    required this.status,
+    required this.type,
+  });
+}
