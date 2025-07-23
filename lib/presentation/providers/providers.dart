09a4c055834565d1803f5e77f89d35c135a00fde
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-23 15:45:46 -0500

    exportado archivo nuevo

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -3,3 +3,5 @@ export 'movies/movies_respository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
 
 export "movies/initial_loading_provider.dart";
+export "actors/actors__by_movie_provider.dart";
+export "actors/actors_repository_provider.dart";
