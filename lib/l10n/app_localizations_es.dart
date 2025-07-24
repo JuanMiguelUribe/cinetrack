207416b300832813a779a00aeb632ee10494cc97
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-24 15:24:16 -0500

    .

---
+++ lib/l10n/app_localizations_es.dart
@@ -0,0 +1,64 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'MovieDex';
+
+  @override
+  String get watchNow => 'Ver ahora';
+
+  @override
+  String get popular => 'Populares';
+
+  @override
+  String get topRated => 'Mejor calificadas';
+
+  @override
+  String get nowPlaying => 'En cine';
+
+  @override
+  String get coomingSoon => 'Próximamente';
+
+  @override
+  String get airingToday => 'Al Aire Hoy';
+
+  @override
+  String get onTheAir => 'En el Aire';
+
+  @override
+  String get movies => 'Películas';
+
+  @override
+  String get tvshows => 'Series de Televisión';
+
+  @override
+  String get always => 'Siempre';
+
+  @override
+  String get homeNav => 'Inicio';
+
+  @override
+  String get categoriasNav => 'Categorías';
+
+  @override
+  String get favsNav => 'Favoritos';
+
+  @override
+  String get loading => 'Cargando';
+
+  @override
+  String get showMore => 'Ver más';
+
+  @override
+  String get showLess => 'Ver menos';
+
+  @override
+  String get cast => 'Reparto';
+}
