dc690d517443dd72c713411e0b424e9497aae92b
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-24 15:23:30 -0500

    Se integro la parte de localizaciones y se creo los archivos para poder cambiar la aplicacion de idioma

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:cinetrack/config/router/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:cinetrack/config/theme/app_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'l10n/app_localizations.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
 
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('es', null);
+  await initializeDateFormatting();
 
   runApp(ProviderScope(child: const MainApp()));
 }
@@ -24,6 +26,17 @@ class MainApp extends StatelessWidget {
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 0, isDarkMode: false).getTheme(),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: const Locale('en'),
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
     );
   }
 }
