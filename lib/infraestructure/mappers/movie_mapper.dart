af4f7da425c158efe7404d6fddeb4a024eea5593
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-25 15:21:14 -0500

    se cambio valores no requeridos o no importantes

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -7,7 +7,7 @@ class MovieMapper {
     adult: moviedb.adult,
     backdropPath: (moviedb.backdropPath != "" && moviedb.backdropPath != null)
         ? "https://image.tmdb.org/t/p/w500${moviedb.backdropPath}"
-        : "https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg",
+        : "https://www.shutterstock.com/shutterstock/videos/1100631657/thumb/4.jpg?ip=x480",
     genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
     id: moviedb.id,
     originalLanguage: moviedb.originalLanguage,
@@ -16,7 +16,7 @@ class MovieMapper {
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != null && moviedb.posterPath != "")
         ? "https://image.tmdb.org/t/p/w500${moviedb.posterPath}"
-        : "https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg",
+        : "https://cdn.displate.com/artwork/857x1200/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg",
 
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
--- lib/infraestructure/mappers/tvshow_mapper.dart
+++ lib/infraestructure/mappers/tvshow_mapper.dart
@@ -11,7 +11,7 @@ class TvshowMapper {
       backdropPath:
           (tvshowdb.backdropPath != "" && tvshowdb.backdropPath != null)
           ? "https://image.tmdb.org/t/p/w500${tvshowdb.backdropPath}"
-          : "https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg",
+          : "https://www.shutterstock.com/shutterstock/videos/1100631657/thumb/4.jpg?ip=x480",
       firstAirDate: tvshowdb.firstAirDate,
       genreIds: tvshowdb.genreIds,
       name: tvshowdb.name,
@@ -21,7 +21,7 @@ class TvshowMapper {
       popularity: tvshowdb.popularity,
       posterPath: (tvshowdb.posterPath != "" && tvshowdb.posterPath != null)
           ? "https://image.tmdb.org/t/p/w500${tvshowdb.posterPath}"
-          : "https://cdn.displate.com/artwork/270x380/2023-02-03/6b806b90ed460362ce845aec44991468_ee90576e764e6e2dc6be65372d967710.jpg",
+          : "https://cdn.displate.com/artwork/857x1200/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg",
       voteAverage: tvshowdb.voteAverage,
       voteCount: tvshowdb.voteCount,
     );
