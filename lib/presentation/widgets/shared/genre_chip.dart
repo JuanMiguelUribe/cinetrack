1a78bbe4e31880c7742936b4cfc1b6b3309c4ec9
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-28 14:17:44 -0500

    chip widget para mostar los generos

---
+++ lib/presentation/widgets/shared/genre_chip.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GenreChip extends StatelessWidget {
+  final String label;
+  final double size;
+  const GenreChip({super.key, required this.label, this.size = 1.0});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12 * size, vertical: 6 * size),
+      margin: EdgeInsets.symmetric(horizontal: 4 * size, vertical: 4 * size),
+      decoration: BoxDecoration(
+        color: colors.secondaryContainer.withOpacity(
+          0.2,
+        ), // fondo oscuro semi-transparente
+        borderRadius: BorderRadius.circular(20 * size), // bordes ovalados
+      ),
+      child: Text(
+        '#$label',
+        style: TextStyle(
+          color: colors.onSurface,
+          fontWeight: FontWeight.w500,
+          fontSize: 10 * size,
+        ),
+      ),
+    );
+  }
+}
