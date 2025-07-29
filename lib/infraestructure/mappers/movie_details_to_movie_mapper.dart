a906f5eabb803691d8f8b6c56263863b3291cf29
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-29 11:48:26 -0500

    se implemento mappers para poder guardar los favoritos de las entidades de detalles a las generales y poder pasarlas al provider y guardarlas en isar

---
+++ lib/infraestructure/mappers/movie_details_to_movie_mapper.dart
@@ -0,0 +1,23 @@
+import 'package:movieflex/domain/entities/movie.dart';
+import 'package:movieflex/domain/entities/movie_details.dart';
+
+extension MovieDetailsMapper on MovieDetails {
+  Movie fromMovieDetailsToMovieEntity() {
+    return Movie(
+      id: id,
+      title: title,
+      overview: overview,
+      posterPath: posterPath,
+      backdropPath: backdropPath,
+      voteAverage: voteAverage,
+      voteCount: voteCount,
+      popularity: popularity,
+      releaseDate: releaseDate,
+      genreIds: genres, // si son objetos
+      adult: adult ?? false,
+      originalLanguage: originalLanguage,
+      originalTitle: originalTitle,
+      video: video ?? false,
+    );
+  }
+}
