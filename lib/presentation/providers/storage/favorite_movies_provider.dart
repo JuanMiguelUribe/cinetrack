98da192e26bfa530998cd9b415ed982a7507f76c
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-29 15:26:09 -0500

    provider para cargar las peliculas y series favoritas del usuario

---
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movieflex/domain/entities/movie.dart';
+import 'package:movieflex/domain/respositories/local_storage_repository.dart';
+import 'package:movieflex/presentation/providers/providers.dart';
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+  StorageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadFavoriteMovies(
+      offset: page * 10,
+    );
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+  }
+}
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+      final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+      return StorageMoviesNotifier(
+        localStorageRepository: localStorageRepository,
+      );
+    });
---
+++ lib/presentation/providers/storage/favorite_tvshow_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movieflex/domain/entities/tv_shows.dart';
+import 'package:movieflex/domain/respositories/local_storage_repository.dart';
+import 'package:movieflex/presentation/providers/providers.dart';
+
+final favoriteTvShowProvider =
+    StateNotifierProvider<TvShowStorageNotifier, Map<int, TvShow>>((ref) {
+      final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+      return TvShowStorageNotifier(
+        localStorageRepository: localStorageRepository,
+      );
+    });
+
+class TvShowStorageNotifier extends StateNotifier<Map<int, TvShow>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+  TvShowStorageNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final tvShows = await localStorageRepository.loadFavoriteTvShows(
+      offset: page * 10,
+    );
+    page++;
+
+    final tempMap = <int, TvShow>{};
+
+    for (final show in tvShows) {
+      tempMap[show.id] = show;
+    }
+
+    state = {...state, ...tempMap};
+  }
+}
