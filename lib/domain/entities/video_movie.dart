71e5d85f75b5d7360a19d931bcff9eab95edd63f
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-30 17:42:54 -0500

    entidad para los videos

---
+++ lib/domain/entities/video_movie.dart
@@ -0,0 +1,13 @@
+class VideoMovie {
+  final String id;
+  final String name;
+  final String youtubeKey;
+  final DateTime publishedAt;
+
+  VideoMovie({
+    required this.id,
+    required this.name,
+    required this.youtubeKey,
+    required this.publishedAt,
+  });
+}
