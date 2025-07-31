135c18f8c3260309e12cb860f47bc5cbbd26e4ce
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-30 20:38:31 -0500

    se cambio la entidad de video, para poder saber el tipo que llega

--- lib/domain/entities/video_movie.dart
+++ lib/domain/entities/video_movie.dart
@@ -2,12 +2,16 @@ class VideoMovie {
   final String id;
   final String name;
   final String youtubeKey;
+  final String site;
+  final String type;
   final DateTime publishedAt;
 
-  VideoMovie({
+  const VideoMovie({
     required this.id,
     required this.name,
     required this.youtubeKey,
+    required this.site,
+    required this.type,
     required this.publishedAt,
   });
 }
