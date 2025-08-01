fe4c51fb66367bf2a7875e1645a8a64c72e1f429
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-07-31 22:26:08 -0500

    se cambio la forma de obtener los videos, y se clasifico por tipo, para hacer la peticion si es pelicula o tv

--- lib/presentation/providers/movies/video_movie_provider.dart
+++ lib/presentation/providers/movies/video_movie_provider.dart
@@ -2,8 +2,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movieflex/domain/entities/video_movie.dart';
 import 'package:movieflex/presentation/providers/movies/movies_respository_provider.dart';
 
-final FutureProviderFamily<List<VideoMovie>, int> videosFromMovieProvider =
-    FutureProvider.family((ref, int movieId) {
-      final movieRepository = ref.watch(movieRepositoryProvider);
-      return movieRepository.getYoutubeVideosById(movieId);
+enum MediaType { movie, tv }
+
+final videosProvider =
+    FutureProvider.family<List<VideoMovie>, ({int id, MediaType type})>((
+      ref,
+      args,
+    ) {
+      final repo = ref.watch(movieRepositoryProvider);
+
+      return args.type == MediaType.movie
+          ? repo.getYoutubeVideosById(args.id)
+          : repo.getYoutubeVideosByIdTvShow(args.id);
     });
