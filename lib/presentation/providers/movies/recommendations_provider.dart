84b1b5e4328ab22bb34366461d7d2af36b17a5c4
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-01 21:42:33 -0500

    notifier y provider para las recomendaciones

---
+++ lib/presentation/providers/movies/recommendations_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movieflex/domain/entities/movie.dart';
+import 'package:movieflex/domain/respositories/movies_repository.dart';
+import 'package:movieflex/presentation/providers/movies/movies_respository_provider.dart';
+
+class RecommendationsNotifier extends StateNotifier<List<Movie>> {
+  final MoviesRepository repository;
+
+  int _currentPage = 1;
+  bool _isLoading = false;
+  final int movieId;
+
+  RecommendationsNotifier({required this.repository, required this.movieId})
+    : super([]) {
+    loadNextPage();
+  }
+
+  Future<void> loadNextPage() async {
+    if (_isLoading) return;
+    _isLoading = true;
+
+    final movies = await repository.getRecomendationsById(
+      movieId,
+      page: _currentPage,
+    );
+    state = [...state, ...movies];
+
+    _currentPage++;
+    _isLoading = false;
+  }
+}
+
+final recommendationsNotifierProvider =
+    StateNotifierProvider.family<RecommendationsNotifier, List<Movie>, int>((
+      ref,
+      movieId,
+    ) {
+      final repository = ref.watch(movieRepositoryProvider);
+      return RecommendationsNotifier(movieId: movieId, repository: repository);
+    });
