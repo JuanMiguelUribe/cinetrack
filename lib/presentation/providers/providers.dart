cf2163311eb17b81dc9d66c3e3cf80055edf83bf
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-03 18:15:25 -0500

    recomendations (movie,tvshow) añadidos al barril

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -17,3 +17,5 @@ export 'bottom_nav/bottom_nav_provider.dart';
 export 'movies/video_movie_provider.dart';
 export 'tvshows/video_tvshow_provider.dart';
 export 'tvshows/tvshows_details_provider.dart';
+export 'movies/recommendations_provider.dart';
+export 'tvshows/recommendations_provider.dart';
