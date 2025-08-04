1a4b4e72c814283ae1d43023bea09bd2349e3322
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-03 22:08:56 -0500

    ruta de tvshow slideshow agregado

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -19,3 +19,4 @@ export 'tvshows/video_tvshow_provider.dart';
 export 'tvshows/tvshows_details_provider.dart';
 export 'movies/recommendations_provider.dart';
 export 'tvshows/recommendations_provider.dart';
+export 'tvshows/tvshow_slideshow_provider.dart';
