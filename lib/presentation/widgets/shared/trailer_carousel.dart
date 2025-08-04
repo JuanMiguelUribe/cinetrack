9e2a3577bf12a266f79f93af3d0f109a62560274
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-03 20:09:53 -0500

    se cambio el valor de color del contenedor por si no hay videos para reproducir

--- lib/presentation/widgets/shared/trailer_carousel.dart
+++ lib/presentation/widgets/shared/trailer_carousel.dart
@@ -53,6 +53,8 @@ class _TrailerCarouselState extends ConsumerState<TrailerCarousel> {
         // final trailers = videos.where((v) => v.type == 'Trailer').toList();
         final limitedVideos = videos.take(20).toList();
 
+        //*If por si no hay videos
+
         if (videos.isEmpty) {
           return _ResultIfVIdeosAreEmpty();
         }
@@ -245,7 +247,12 @@ class _ResultIfVIdeosAreEmpty extends StatelessWidget {
               borderRadius: BorderRadius.circular(20),
               child: Container(
                 decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 226, 226, 226).withAlpha(10),
+                  color: const Color.fromARGB(
+                    255,
+                    226,
+                    226,
+                    226,
+                  ).withAlpha(100),
                 ),
                 child: AspectRatio(
                   aspectRatio: 16 / 9,
