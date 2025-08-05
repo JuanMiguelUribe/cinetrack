d8639b2da985a29c2ebb5dc798100199d7d83509
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-04 21:16:27 -0500

    el main se convirtio a consumerwidget para que lea el provider del tema

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:movieflex/config/theme/app_theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:movieflex/presentation/providers/providers.dart';
 import 'l10n/app_localizations.dart';
 
 Future<void> main() async {
@@ -17,15 +18,16 @@ Future<void> main() async {
   runApp(ProviderScope(child: const MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final AppTheme apptheme = ref.watch(themeNotifierProvider);
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 0, isDarkMode: false).getTheme(),
+      theme: apptheme.getTheme(),
       localizationsDelegates: [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
