47b651b7d2e747f011bb22a3cfdb7d3ba868614b
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-05 20:15:44 -0500

    .

--- lib/presentation/providers/movies/initial_loading_provider.dart
+++ lib/presentation/providers/movies/initial_loading_provider.dart
@@ -10,7 +10,6 @@ final initialLoadingProvider = Provider<bool>((ref) {
   final step6 = ref.watch(onTheAirTvShowProvider).isEmpty;
   final step7 = ref.watch(popularTvShowProvider).isEmpty;
   final step8 = ref.watch(topRatedTvShowProvider).isEmpty;
-
   // Si alguno de los pasos está vacío, significa que aún no se ha cargado
   if (step1 || step2 || step3 || step4 || step5 || step6 || step7 || step8) {
     return true; // Aún estamos cargando
