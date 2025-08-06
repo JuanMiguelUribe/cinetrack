05cbdde3050f5e7ef0f00d20c27b79df9bdf7841
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-06 16:52:57 -0500

    se cambio el dnombre de categories a discover

--- lib/presentation/views/home_views/views.dart
+++ lib/presentation/views/home_views/views.dart
@@ -1,3 +1,3 @@
 export 'package:movieflex/presentation/views/home_views/home_view.dart';
 export 'package:movieflex/presentation/views/home_views/favorites_view.dart';
-export 'package:movieflex/presentation/views/home_views/categories_view.dart';
+export 'package:movieflex/presentation/views/home_views/discover_view.dart';
