b40c1f9b6da0dabca7540ed97fc3d8ab6c3f2f8e
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-10 22:29:02 -0500

    se cambio el init state para que carguen los widgets cada vez que se hace reload

--- lib/presentation/views/home_views/discover_view.dart
+++ lib/presentation/views/home_views/discover_view.dart
@@ -6,6 +6,7 @@ import 'package:intl/intl.dart';
 import 'package:movieflex/config/helpers/localizations_helper.dart';
 import 'package:movieflex/config/theme/app_text_styles.dart';
 import 'package:movieflex/domain/entities/movie.dart';
+import 'package:movieflex/domain/entities/tv_shows.dart';
 import 'package:movieflex/l10n/app_localizations.dart';
 import 'package:movieflex/presentation/providers/providers.dart';
 import 'package:movieflex/presentation/widgets/widgets.dart';
@@ -18,7 +19,11 @@ class DiscoverMoviesView extends ConsumerStatefulWidget {
 }
 
 class DiscoverMoviesViewState extends ConsumerState<DiscoverMoviesView> {
-  PageController _pageController = PageController(
+  PageController _moviesPageController = PageController(
+    viewportFraction: 0.8,
+    initialPage: 10,
+  );
+  PageController _seriesPageController = PageController(
     viewportFraction: 0.8,
     initialPage: 10,
   );
@@ -26,27 +31,49 @@ class DiscoverMoviesViewState extends ConsumerState<DiscoverMoviesView> {
 
   @override
   void dispose() {
-    _pageController.dispose();
+    _moviesPageController.dispose();
+    _seriesPageController.dispose();
     super.dispose();
   }
 
   void _resetPageController() {
-    _pageController.dispose();
-    _pageController = PageController(
-      viewportFraction: 0.8,
-      initialPage: 10,
-    ); // Nueva instancia
-    setState(() {});
+    setState(() {
+      _moviesPageController.dispose();
+      _seriesPageController.dispose();
+
+      _moviesPageController = PageController(
+        viewportFraction: 0.8,
+        initialPage: 10,
+      );
+      _seriesPageController = PageController(
+        viewportFraction: 0.8,
+        initialPage: 10,
+      ); // N
+    });
   }
 
   Future<void> _onRefresh() async {
-    await ref.read(discoverMoviesProvider.notifier).loadNextRandomPage();
+    ref.invalidate(discoverMoviesProvider);
+    ref.invalidate(discoverSeriesProvider);
+
     _resetPageController();
+
+    await ref.read(discoverMoviesProvider.notifier).loadNextRandomPage();
+    await ref.read(discoverSeriesProvider.notifier).loadNextRandomPage();
+
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+    //   if (selectedIndex == 0) {
+    //     _moviesPageController.jumpToPage(10);
+    //   } else {
+    //     _seriesPageController.jumpToPage(10);
+    //   }
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     final discoverMovies = ref.watch(discoverMoviesProvider);
+    final discoverSeries = ref.watch(discoverSeriesProvider);
     final size = MediaQuery.of(context).size;
     final isLoading = ref.watch(initialLoadingDiscoverProvider);
 
@@ -69,21 +96,41 @@ class DiscoverMoviesViewState extends ConsumerState<DiscoverMoviesView> {
               ),
             ),
 
-            // Películas
-            SliverToBoxAdapter(
-              child: _PageSwiper(
-                size: size,
-                pageController: _pageController, // controlador independiente
-                discoverMovies: discoverMovies,
-                loadNextPage: () => ref
-                    .read(discoverMoviesProvider.notifier)
-                    .loadNextRandomPageAdded(),
-                loadNextPageBackward: () => ref
-                    .read(discoverMoviesProvider.notifier)
-                    .loadPreviousRandomPageAdded(),
+            if (selectedIndex == 0) ...[
+              SliverToBoxAdapter(
+                child: _PageSwiper(
+                  type: MediaType.movie,
+                  size: size,
+                  pageController:
+                      _moviesPageController, // controlador independiente
+                  discoverMovies: discoverMovies,
+                  loadNextPage: () => ref
+                      .read(discoverMoviesProvider.notifier)
+                      .loadNextRandomPageAdded(),
+                  loadNextPageBackward: () => ref
+                      .read(discoverMoviesProvider.notifier)
+                      .loadPreviousRandomPageAdded(),
+                ),
               ),
-            ),
+            ] else ...[
+              SliverToBoxAdapter(
+                child: _PageSwiperSeries(
+                  type: MediaType.tv,
+                  size: size,
+                  pageController:
+                      _seriesPageController, // controlador independiente
+                  discoverSeries: discoverSeries,
+                  loadNextPage: () => ref
+                      .read(discoverSeriesProvider.notifier)
+                      .loadNextRandomPageAdded(),
+                  loadNextPageBackward: () => ref
+                      .read(discoverSeriesProvider.notifier)
+                      .loadPreviousRandomPageAdded(),
+                ),
+              ),
+            ],
 
+            //* Películas
             const SliverToBoxAdapter(child: SizedBox(height: 30)),
 
             // Series
@@ -111,7 +158,7 @@ class DiscoverMoviesViewState extends ConsumerState<DiscoverMoviesView> {
 
 class _PageSwiper extends ConsumerStatefulWidget {
   const _PageSwiper({
-    super.key,
+    required this.type,
     required this.size,
     required this.pageController,
     required this.discoverMovies,
@@ -124,6 +171,7 @@ class _PageSwiper extends ConsumerStatefulWidget {
   final List<Movie> discoverMovies;
   final VoidCallback? loadNextPage;
   final VoidCallback? loadNextPageBackward;
+  final MediaType type;
   @override
   ConsumerState<_PageSwiper> createState() => _PageSwiperState();
 }
@@ -135,6 +183,7 @@ class _PageSwiperState extends ConsumerState<_PageSwiper> {
   @override
   void initState() {
     super.initState();
+
     _pageListener = () {
       final page = widget.pageController.page?.round() ?? 10;
 
@@ -243,6 +292,21 @@ class _PageSwiperState extends ConsumerState<_PageSwiper> {
                             movie: movie,
                             isFavoriteFuture: isFavoriteFuture,
                             colors: colors,
+                            onPressed: () async {
+                              await
+                              // ref
+                              //     .read(localStorageRepositoryProvider)
+                              //     .toggleFavoriteMovie(movie);
+                              ref
+                                  .read(favoriteMoviesProvider.notifier)
+                                  .toggleFavorite(movie);
+                              ref.invalidate(
+                                isFavoriteProvider((
+                                  type: 'movie',
+                                  id: movie.id,
+                                )),
+                              );
+                            },
                           ),
                     // SizedBox(height: 8),
                     //*TITULO DE LA PELICULA
@@ -286,9 +350,245 @@ class _PageSwiperState extends ConsumerState<_PageSwiper> {
           ),
         ),
 
-        const SizedBox(height: 16),
+        const SizedBox(height: 0),
         SizedBox(
-          height: 40,
+          height: 30,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(20, (index) {
+              final isSelected = index == (_currentPage % 20);
+
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: isSelected ? 20 : 8,
+                height: 8,
+                margin: const EdgeInsets.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: isSelected
+                      ? colors.primary
+                      : colors.onSurface.withAlpha(100),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              );
+            }),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _PageSwiperSeries extends ConsumerStatefulWidget {
+  const _PageSwiperSeries({
+    required this.type,
+    required this.size,
+    required this.pageController,
+    required this.discoverSeries,
+    this.loadNextPage,
+    this.loadNextPageBackward,
+  });
+
+  final Size size;
+  final PageController pageController;
+  final List<TvShow> discoverSeries;
+  final VoidCallback? loadNextPage;
+  final VoidCallback? loadNextPageBackward;
+  final MediaType type;
+  @override
+  ConsumerState<_PageSwiperSeries> createState() => _PageSwiperSeriesState();
+}
+
+class _PageSwiperSeriesState extends ConsumerState<_PageSwiperSeries> {
+  int _currentPage = 10;
+  late final VoidCallback _pageListener;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageListener = () {
+      final page = widget.pageController.page?.round() ?? 10;
+
+      if (mounted && page != _currentPage) {
+        setState(() {
+          _currentPage = page;
+        });
+      }
+
+      // Carga hacia adelante
+      if (widget.loadNextPage != null &&
+          page >= widget.discoverSeries.length - 1) {
+        widget.loadNextPage!();
+      }
+
+      // Carga hacia atrás
+      if (page <= 0) {
+        final previousItemCount = 20; // o la cantidad que vayas a insertar
+        final viewportFraction = widget.pageController.viewportFraction;
+        final pageWidth =
+            widget.pageController.position.viewportDimension * viewportFraction;
+        final offsetBefore = widget.pageController.offset;
+
+        widget.loadNextPageBackward!();
+        final offsetAfter = offsetBefore + (pageWidth * previousItemCount);
+
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          Future.delayed(Duration(seconds: 1));
+
+          if (mounted) {
+            widget.pageController.jumpTo(offsetAfter);
+          }
+        });
+      }
+    };
+    widget.pageController.addListener(_pageListener);
+  }
+
+  @override
+  void dispose() {
+    widget.pageController.removeListener(_pageListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final textStyles = Theme.of(context).textTheme;
+    final tvItems = widget.discoverSeries
+        .map(
+          (tv) => Movie(
+            id: tv.id,
+            title: tv.name,
+            posterPath: tv.posterPath!,
+            overview: tv.overview ?? "",
+            popularity: tv.voteAverage,
+            adult: false,
+            genreIds: tv.genreIds,
+            originalLanguage: tv.originalLanguage ?? "",
+            originalTitle: tv.originalName ?? "",
+            video: false,
+            voteAverage: tv.voteAverage,
+            voteCount: tv.voteCount,
+          ),
+        )
+        .toList();
+    return Column(
+      children: [
+        SizedBox(
+          height: widget.size.width * 1.38,
+          child: PageView.builder(
+            controller: widget.pageController,
+            itemCount: widget.discoverSeries.length,
+            physics: RangeMaintainingScrollPhysics(),
+            padEnds: true,
+            itemBuilder: (context, index) {
+              final serie = widget.discoverSeries[index];
+              final movie = tvItems[index];
+              final loc = AppLocalizations.of(context)!;
+              final isFavoriteFuture = ref.watch(
+                isFavoriteProvider((type: 'tv', id: movie.id)),
+              );
+
+              return GestureDetector(
+                onTap: () {
+                  context.push('/tvshow/${movie.id}');
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AnimatedBuilder(
+                      animation: widget.pageController,
+                      builder: (context, child) {
+                        double value = 1.0;
+                        if (widget.pageController.position.haveDimensions) {
+                          value = widget.pageController.page! - index;
+                          value = (1 - (value.abs() * 0.3)).clamp(0.0, 1.0);
+                        }
+
+                        return Center(
+                          child: SizedBox(
+                            height: Curves.easeOut.transform(value) * 410,
+                            width: Curves.easeOut.transform(value) * 370,
+                            child: child,
+                          ),
+                        );
+                      },
+                      //*MOTRAR POSTER MOVIE/TV
+                      child: _PosterPathWidget(movie: movie),
+                    ),
+
+                    //*FAVORITOS, GENEROS Y RATING
+                    const SizedBox(height: 0),
+                    movie.genreIds.isNotEmpty
+                        ? _FavoriteGendersAndRatingWidget(
+                            ref: ref,
+                            movie: movie,
+                            isFavoriteFuture: isFavoriteFuture,
+                            colors: colors,
+                            loc: loc,
+                          )
+                        : _FavoriteAndRatingWidget(
+                            ref: ref,
+                            movie: movie,
+                            isFavoriteFuture: isFavoriteFuture,
+                            colors: colors,
+                            onPressed: () async {
+                              await
+                              // ref
+                              //     .read(localStorageRepositoryProvider)
+                              //     .toggleFavoriteMovie(movie);
+                              ref
+                                  .read(favoriteTvShowProvider.notifier)
+                                  .toggleFavorite(serie);
+                              ref.invalidate(
+                                isFavoriteProvider((type: 'tv', id: movie.id)),
+                              );
+                            },
+                          ),
+                    // SizedBox(height: 8),
+                    //*TITULO DE LA PELICULA
+                    Text(
+                      movie.title,
+                      style: AppTextStyles.styleForTitleContentDiscover(
+                        context,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    //*FECHA
+                    Text(
+                      "${movie.releaseDate != null ? DateFormat('d MMMM y').format(movie.releaseDate!) : AppLocalizations.of(context)!.unknownDate}",
+                      style: textStyles.bodySmall?.copyWith(
+                        color: colors.onSurface.withAlpha(150),
+                      ),
+                    ),
+                    if (movie.adult)
+                      Center(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 6,
+                            vertical: 2,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          child: const Text(
+                            '+18',
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+
+        const SizedBox(height: 0),
+        SizedBox(
+          height: 30,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: List.generate(20, (index) {
@@ -416,17 +716,18 @@ class _SegmentedControlHeaderDiscover extends SliverPersistentHeaderDelegate {
 
 class _FavoriteAndRatingWidget extends StatelessWidget {
   const _FavoriteAndRatingWidget({
-    super.key,
     required this.ref,
     required this.movie,
     required this.isFavoriteFuture,
     required this.colors,
+    this.onPressed,
   });
 
   final WidgetRef ref;
   final Movie movie;
   final AsyncValue<bool> isFavoriteFuture;
   final ColorScheme colors;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -436,14 +737,7 @@ class _FavoriteAndRatingWidget extends StatelessWidget {
         Spacer(),
 
         IconButton(
-          onPressed: () async {
-            await
-            // ref
-            //     .read(localStorageRepositoryProvider)
-            //     .toggleFavoriteMovie(movie);
-            ref.read(favoriteMoviesProvider.notifier).toggleFavorite(movie);
-            ref.invalidate(isFavoriteProvider((type: 'movie', id: movie.id)));
-          },
+          onPressed: onPressed,
           icon: isFavoriteFuture.when(
             loading: () => CircularProgressIndicator(strokeWidth: 2),
             data: (isFavorite) => isFavorite
@@ -474,7 +768,6 @@ class _FavoriteAndRatingWidget extends StatelessWidget {
 
 class _FavoriteGendersAndRatingWidget extends StatelessWidget {
   const _FavoriteGendersAndRatingWidget({
-    super.key,
     required this.ref,
     required this.movie,
     required this.isFavoriteFuture,
@@ -557,7 +850,7 @@ class _FavoriteGendersAndRatingWidget extends StatelessWidget {
 }
 
 class _PosterPathWidget extends StatelessWidget {
-  const _PosterPathWidget({super.key, required this.movie});
+  const _PosterPathWidget({required this.movie});
 
   final Movie movie;
 
