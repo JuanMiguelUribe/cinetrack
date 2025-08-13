b98fb98b159c380ba83a39e8af3ad872b9e8a733
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-12 21:40:54 -0500

    provides para obtener los detalles de temporadas de las series

--- lib/presentation/providers/tvshows/seasons_provider.dart
+++ lib/presentation/providers/tvshows/seasons_provider.dart
@@ -3,12 +3,16 @@ import 'package:movieflex/domain/entities/seasons.dart';
 import 'package:movieflex/presentation/providers/providers.dart';
 
 /// Tipo de callback que usará el notifier
-final tvShowSeasonsProvider =
-    FutureProvider.family<List<Season>, Map<String, dynamic>>((ref, params) {
-      final repository = ref.watch(tvshowsRepositoryProvider);
+final tvShowSeasonsProvider = FutureProvider.family<Season, (String, int)>((
+  ref,
+  params,
+) {
+  final repository = ref.watch(tvshowsRepositoryProvider);
 
-      final id = params['id'] as String;
-      final season = params['season'] as int;
+  final id = params.$1;
+  final season = params.$2; //
 
-      return repository.tvShowSeasons(id, season);
-    });
+  return repository.tvShowSeasons(id, season);
+});
+
+final selectedSeasonProvider = StateProvider.autoDispose<int>((ref) => 1);
