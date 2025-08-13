63e04d57770ede72bcf7cae8a432674dc33b92d0
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-12 21:38:50 -0500

    se agrego el mapper para la entidad de los detalles de actores

--- lib/infraestructure/mappers/actor_mapper.dart
+++ lib/infraestructure/mappers/actor_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:movieflex/domain/entities/actor.dart';
+import 'package:movieflex/infraestructure/models/movieDb/actors_details_response.dart';
 import 'package:movieflex/infraestructure/models/movieDb/actorstv_response.dart';
 import 'package:movieflex/infraestructure/models/movieDb/credits_response.dart';
 
@@ -28,4 +29,24 @@ class ActorMapper {
       knownForDepartment: cast.knownForDepartment,
     );
   }
+
+  static PersonDetailsEntity actorDetailToEntity(
+    ActorDbResponse actor,
+  ) => PersonDetailsEntity(
+    id: actor.id,
+    name: actor.name,
+    alsoKnownAs: actor.alsoKnownAs,
+    biography: actor.biography,
+    birthday: actor.birthday,
+    deathday: actor.deathday,
+    gender: actor.gender,
+    homepage: actor.homepage,
+    imdbId: actor.imdbId,
+    knownForDepartment: actor.knownForDepartment,
+    placeOfBirth: actor.placeOfBirth,
+    popularity: actor.popularity,
+    profilePath: actor.profilePath != null
+        ? "https://image.tmdb.org/t/p/w500${actor.profilePath}"
+        : "https://assets.mycast.io/actor_images/actor-a-unknown-voice-actor-745520_small.jpg?1682266765",
+  );
 }
