6ecf6a729d805ef3a09bf57219b2030363f5838f
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-13 17:21:12 -0500

    se creo el abstract repository para recibir los creditos de los actores

--- lib/domain/respositories/actors_repository.dart
+++ lib/domain/respositories/actors_repository.dart
@@ -4,4 +4,5 @@ abstract class ActorsRepository {
   Future<List<Actor>> getActorsByMovie(String movieId);
   Future<List<Actor>> getActorsByShow(String tvshowId);
   Future<PersonDetailsEntity> getActorById(String id);
+  Future<ActorCredit> getCreditByActor(String id);
 }
