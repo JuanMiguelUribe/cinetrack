134e6382d36c32174e38bfdeabd479ec3c930c2d
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-13 17:20:51 -0500

    se creo la entidad para recibir los creditos de los actores

--- lib/domain/entities/actor.dart
+++ lib/domain/entities/actor.dart
@@ -1,3 +1,5 @@
+import 'package:movieflex/infraestructure/models/movieDb/credits_actor_response.dart';
+
 class MovieCreditsEntity {
   final int id;
   final List<Actor> cast;
@@ -34,9 +36,9 @@ class PersonDetailsEntity {
   final int id;
   final String name;
   final String? profilePath;
-  final String? biography;
+  final String biography;
   final DateTime? birthday;
-  final String? deathday;
+  final DateTime? deathday;
   final int? gender; // 1 = mujer, 2 = hombre, 0/3 = otro
   final String? homepage;
   final String? imdbId;
@@ -49,7 +51,7 @@ class PersonDetailsEntity {
     required this.id,
     required this.name,
     this.profilePath,
-    this.biography,
+    required this.biography,
     this.birthday,
     this.deathday,
     this.gender,
@@ -59,5 +61,21 @@ class PersonDetailsEntity {
     this.placeOfBirth,
     this.popularity,
     this.alsoKnownAs = const [],
+  });
+}
+
+class ActorCredit {
+  final int id;
+  final String title;
+  final MediaType mediaType; // "movie" o "tv"
+  final double voteAverage;
+  final String? posterUrl;
+
+  const ActorCredit({
+    required this.id,
+    required this.title,
+    required this.mediaType,
+    required this.voteAverage,
+    this.posterUrl,
   });
 }
