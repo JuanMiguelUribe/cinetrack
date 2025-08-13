a7fac873942ec46d4f524491c9dce051df972d4a
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-12 21:37:01 -0500

    se agrego la entidad para los detalles de cada actor

--- lib/domain/entities/actor.dart
+++ lib/domain/entities/actor.dart
@@ -29,3 +29,35 @@ class Actor {
     this.knownForDepartment,
   });
 }
+
+class PersonDetailsEntity {
+  final int id;
+  final String name;
+  final String? profilePath;
+  final String? biography;
+  final DateTime? birthday;
+  final String? deathday;
+  final int? gender; // 1 = mujer, 2 = hombre, 0/3 = otro
+  final String? homepage;
+  final String? imdbId;
+  final String? knownForDepartment;
+  final String? placeOfBirth;
+  final double? popularity;
+  final List<String> alsoKnownAs;
+
+  PersonDetailsEntity({
+    required this.id,
+    required this.name,
+    this.profilePath,
+    this.biography,
+    this.birthday,
+    this.deathday,
+    this.gender,
+    this.homepage,
+    this.imdbId,
+    this.knownForDepartment,
+    this.placeOfBirth,
+    this.popularity,
+    this.alsoKnownAs = const [],
+  });
+}
