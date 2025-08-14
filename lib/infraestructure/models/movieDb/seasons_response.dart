e89be513f4ff157149a540a37e6ee937a95ca182
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-14 16:07:05 -0500

    se hace el ignore

--- lib/infraestructure/models/movieDb/seasons_response.dart
+++ lib/infraestructure/models/movieDb/seasons_response.dart
@@ -205,14 +205,24 @@ class Crew {
 }
 
 enum Department {
+  // ignore: constant_identifier_names
   ACTING,
+  // ignore: constant_identifier_names
   ART,
+  // ignore: constant_identifier_names
   CREATOR,
+  // ignore: constant_identifier_names
   CREW,
+  // ignore: constant_identifier_names
   DIRECTING,
+  // ignore: constant_identifier_names
   EDITING,
+  // ignore: constant_identifier_names
+  // ignore: constant_identifier_names
   PRODUCTION,
+  // ignore: constant_identifier_names
   VISUAL_EFFECTS,
+  // ignore: constant_identifier_names
   WRITING,
 }
 
