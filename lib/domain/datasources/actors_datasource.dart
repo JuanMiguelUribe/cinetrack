d1e57f557bb9290e6862a8ca92afa4b25f20c69c
Juan Miguel Uribe <jmuribe04@gmail.com>
2025-08-13 21:17:55 -0500

    se paso a lista

--- lib/domain/datasources/actors_datasource.dart
+++ lib/domain/datasources/actors_datasource.dart
@@ -4,5 +4,5 @@ abstract class ActorsDatasource {
   Future<List<Actor>> getActorsByMovie(String movieId);
   Future<List<Actor>> getActorsByShow(String tvshowId);
   Future<PersonDetailsEntity> getActorById(String id);
-  Future<ActorCredit> getCreditByActor(String id);
+  Future<List<ActorCredit>> getCreditByActor(String id);
 }
